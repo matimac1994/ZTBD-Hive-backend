927702f240986254b6049d5d0c1ff036a2413e2e
Mati <matiwsrh1994@gmail.com>
2018-06-19 21:30:43 +0200

    Init

---
+++ src/main/java/com/maciejak/ztbdhive/ZtbdHiveApplication.java
@@ -0,0 +1,12 @@
+package com.maciejak.ztbdhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtbdHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZtbdHiveApplication.class, args);
+	}
+}
---
+++ src/test/java/com/maciejak/ztbdhive/ZtbdHiveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maciejak.ztbdhive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZtbdHiveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
