eac3e8780e81dc874c90fa4c9b1c48cdf8c12707
Kordian Kurdziel <kordiankurdziel@gmail.com>
2018-06-20 12:03:02 +0200

    userDao impl add, edit, delete

--- src/main/java/com/maciejak/ztbdhive/dao/UserDao.java
+++ src/main/java/com/maciejak/ztbdhive/dao/UserDao.java
@@ -1,11 +1,17 @@
 package com.maciejak.ztbdhive.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
 import com.maciejak.ztbdhive.model.User;
@@ -13,19 +19,19 @@ import com.maciejak.ztbdhive.model.User;
 @Component
 public class UserDao implements IUserDao {
 
-    @Autowired
-    @Qualifier("hiveJdbcTemplate")
-    private JdbcTemplate hiveJdbcTemplate;
+	@Autowired
+	@Qualifier("hiveJdbcTemplate")
+	private JdbcTemplate hiveJdbcTemplate;
 
-    @Override
-    public List<User> getAllUsers(){
-    	List<User> users = new ArrayList<>();
-    	// TODO
-    	
-        StringBuilder builder = new StringBuilder();
+	@Override
+	public List<User> getAllUsers() {
+		List<User> users = new ArrayList<>();
+		// TODO
 
-        return users;
-    }
+		StringBuilder builder = new StringBuilder();
+
+		return users;
+	}
 
 	@Override
 	public User getUser(Long id) {
@@ -35,20 +41,47 @@ public class UserDao implements IUserDao {
 
 	@Override
 	public User addUser(User user) {
-		// TODO Auto-generated method stub
-		return null;
+		final String INSERT_SQL = "insert into student (firstName, lastName, age, city) values(?, ?, ?, ?)";
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		hiveJdbcTemplate.update(new PreparedStatementCreator() {
+			public PreparedStatement createPreparedStatement(
+					Connection connection) throws SQLException {
+				PreparedStatement ps = connection.prepareStatement(INSERT_SQL,
+						new String[] { "id" });
+				ps.setString(1, user.getFirstName());
+				ps.setString(2, user.getLastName());
+				ps.setInt(3, user.getAge());
+				ps.setString(4, user.getCity());
+				return ps;
+			}
+		}, keyHolder);
+
+		return getUser(keyHolder.getKey().longValue());
 	}
 
 	@Override
 	public User editUser(User user) {
-		// TODO Auto-generated method stub
-		return null;
+		final String UPDATE_SQL = "update student set firstName = ?, lastName = ?, age = ?, city = ? where id = ?";
+		hiveJdbcTemplate.update(new PreparedStatementCreator() {
+			public PreparedStatement createPreparedStatement(
+					Connection connection) throws SQLException {
+				PreparedStatement ps = connection.prepareStatement(UPDATE_SQL);
+				ps.setString(1, user.getFirstName());
+				ps.setString(2, user.getLastName());
+				ps.setInt(3, user.getAge());
+				ps.setString(4, user.getCity());
+				ps.setLong(5, user.getId());
+				return ps;
+			}
+		});
+
+		return getUser(user.getId());
 	}
 
 	@Override
 	public void deleteUser(Long id) {
-		// TODO Auto-generated method stub
-		
+		String query = "delete from student where id = " + id;
+		hiveJdbcTemplate.update(query);
 	}
 
 	@Override
