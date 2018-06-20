72b2abceabdcc52e0f13bd40e7d7a86fba78b79a
Mati <maciejak.dev@gmail.com>
2018-06-20 20:43:11 +0200

    backend final

--- src/main/java/com/maciejak/ztbdhive/config/HiveConfig.java
+++ src/main/java/com/maciejak/ztbdhive/config/HiveConfig.java
@@ -10,10 +10,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 
 @Configuration
@@ -23,11 +20,6 @@ public class HiveConfig {
     @Value("${hive.driver-class-name}") String hiveDriver;
     @Value("${hive.username}") String hiveUsername;
     @Value("${hive.password}") String hivePassword;
-    
-    @Value("${hive2.url}") String hiveUrl2;
-    @Value("${hive2.driver-class-name}") String hiveDriver2;
-    @Value("${hive2.username}") String hiveUsername2;
-    @Value("${hive2.password}") String hivePassword2;
 
     @Bean(name = "hiveJdbcDataSource")
     @Qualifier("hiveJdbcDataSource")
@@ -44,9 +36,7 @@ public class HiveConfig {
 
         Connection connection = dataSource.getConnection();
         Statement statement = connection.createStatement();
-        statement.execute("drop table if EXISTS tabelka");
-        statement.execute("create table tabelka (key int, value string)");
-
+        statement.execute("CREATE TABLE IF NOT EXISTS student (id int, firstName String, lastName String, age int, city String) stored as sequencefile");
         return dataSource;
 
     }
@@ -55,33 +45,4 @@ public class HiveConfig {
     public JdbcTemplate hiveJdbcTemplate(@Qualifier("hiveJdbcDataSource") DataSource dataSource) {
         return new JdbcTemplate(dataSource);
     }
-
-//    @Bean(name = "hiveJdbcDataSource")
-//    @Qualifier("hiveJdbcDataSource")
-//    public DataSource dataSource() throws SQLException {
-//
-//        System.out.println(hiveUrl);
-//        try {
-//			Class.forName("com.mysql.jdbc.Driver");
-//		} catch (ClassNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//        Connection conn = null;
-//        com.mysql.jdbc.Driver d = new com.mysql.jdbc.Driver();
-//        HiveDriver driver = new HiveDriver();
-//        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-//        dataSource.setUrl(hiveUrl);
-//        dataSource.setDriver(d);
-//        dataSource.setPassword(hivePassword);
-//        dataSource.setUsername(hiveUsername);
-//
-//        Connection connection = dataSource.getConnection();
-//        Statement statement = connection.createStatement();
-//        statement.execute("CREATE TABLE IF NOT EXISTS student (id int, firstName varchar(100), lastName varchar(100), age int, city varchar(100));");
-//        statement.execute("insert into student (id, firstName, lastName, age, city) values(1, 'Mati', 'Maciejak', 21, 'krk')");
-//        return dataSource;
-//
-//    }
-    
 }
--- src/main/java/com/maciejak/ztbdhive/controller/UserController.java
+++ src/main/java/com/maciejak/ztbdhive/controller/UserController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/user")
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
@@ -17,29 +18,36 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping
+    @GetMapping("/all")
     @ResponseBody
     public ResponseEntity<List<User>> getAllUsers(){
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
-    @PostMapping
+    @PostMapping()
     @ResponseBody
     public ResponseEntity<User> saveUser(@RequestBody User user){
+        return ResponseEntity.ok(userService.createOrUpdateUser(user));
+    }
+
+    @PutMapping()
+    @ResponseBody
+    public ResponseEntity<User> update(@RequestBody User user){
         return ResponseEntity.ok(userService.createOrUpdateUser(user));
     }
 
     @GetMapping("/{id}")
     @ResponseBody
     public ResponseEntity<User> getAUserById(
-            @PathVariable("id") Integer userId
+            @PathVariable(value = "id") Integer userId
     ){
         return ResponseEntity.ok(userService.getUserById(userId));
     }
 
     @DeleteMapping("/{id}")
+    @ResponseBody
     public ResponseEntity<Void> deletetAUserById(
-            @PathVariable("id") Integer userId
+            @PathVariable(value = "id") Integer userId
     ){
         userService.deleteUserById(userId);
         return ResponseEntity.ok().build();
