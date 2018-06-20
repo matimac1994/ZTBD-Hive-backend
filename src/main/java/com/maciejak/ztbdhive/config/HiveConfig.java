248e4f4c064fd77cb581acf9f6820ea887dc0b14
Kordian Kurdziel <kordiankurdziel@gmail.com>
2018-06-20 13:15:48 +0200

    hive2

--- src/main/java/com/maciejak/ztbdhive/config/HiveConfig.java
+++ src/main/java/com/maciejak/ztbdhive/config/HiveConfig.java
@@ -23,6 +23,11 @@ public class HiveConfig {
     @Value("${hive.driver-class-name}") String hiveDriver;
     @Value("${hive.username}") String hiveUsername;
     @Value("${hive.password}") String hivePassword;
+    
+    @Value("${hive2.url}") String hiveUrl2;
+    @Value("${hive2.driver-class-name}") String hiveDriver2;
+    @Value("${hive2.username}") String hiveUsername2;
+    @Value("${hive2.password}") String hivePassword2;
 
     @Bean(name = "hiveJdbcDataSource")
     @Qualifier("hiveJdbcDataSource")
@@ -39,9 +44,9 @@ public class HiveConfig {
 
         Connection connection = dataSource.getConnection();
         Statement statement = connection.createStatement();
-//        statement.execute("drop table if exists student");
-        statement.execute("CREATE TABLE IF NOT EXISTS student (id int, firstName String, lastName String, age int, city String)");
-        statement.execute("insert into student (id, firstName, lastName, age, city) values(1, 'Mati', 'Maciejak', 21, 'krk')");
+        statement.execute("drop table if EXISTS tabelka");
+        statement.execute("create table tabelka (key int, value string)");
+
         return dataSource;
 
     }
@@ -51,4 +56,32 @@ public class HiveConfig {
         return new JdbcTemplate(dataSource);
     }
 
+//    @Bean(name = "hiveJdbcDataSource")
+//    @Qualifier("hiveJdbcDataSource")
+//    public DataSource dataSource() throws SQLException {
+//
+//        System.out.println(hiveUrl);
+//        try {
+//			Class.forName("com.mysql.jdbc.Driver");
+//		} catch (ClassNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//        Connection conn = null;
+//        com.mysql.jdbc.Driver d = new com.mysql.jdbc.Driver();
+//        HiveDriver driver = new HiveDriver();
+//        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+//        dataSource.setUrl(hiveUrl);
+//        dataSource.setDriver(d);
+//        dataSource.setPassword(hivePassword);
+//        dataSource.setUsername(hiveUsername);
+//
+//        Connection connection = dataSource.getConnection();
+//        Statement statement = connection.createStatement();
+//        statement.execute("CREATE TABLE IF NOT EXISTS student (id int, firstName varchar(100), lastName varchar(100), age int, city varchar(100));");
+//        statement.execute("insert into student (id, firstName, lastName, age, city) values(1, 'Mati', 'Maciejak', 21, 'krk')");
+//        return dataSource;
+//
+//    }
+    
 }
